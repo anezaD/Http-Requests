3dbb3817a41070bf44e8bb264d4facbeb0a84cc3
Aneza <aneza.dok@gmail.com>
2023-12-31 18:55:56 +0200

    Latest Version

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,17 +6,29 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState({});
 
   useEffect(() => {
     async function fechPlaces() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      console.log("isFetching 1--->", isFetching);
-      const resData = await response.json();
 
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+      try {
+        const response = await fetch('http://localhost:3000/placefffffs');
+        const resData = await response.json();
+        // response.ok -- 200 or 300
+        // !response.ok -- 400 or 500
+
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+
+        setError({ message: error.message || 'Could not fetch places, you are kindly asked to wait for a moment' })
+      }
       setIsFetching(false);
-      console.log("isFetching 2--->", isFetching);
+      console.log("isFetching 2 --->", isFetching);
 
     }
     fechPlaces();
